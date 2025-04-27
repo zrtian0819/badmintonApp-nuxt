45c39e7538ff3fd4487faca7eebcf80fbcd5f3c5
Joe_Zhang <cjt3399@gmail.com>
2025-04-27 21:03:27 +0800

    Deploy static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/badmintonApp-nuxt/' // 替換為你的倉庫名稱
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint','@nuxtjs/tailwindcss']
